b2dc73ab297705a0ea14c332c1230f7e580d03b3
AnarAgaev <anar.n.agaev@gmail.com>
2023-02-01 16:27:54 +0300

    Sested React dev-tools profiler

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, Profiler } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
@@ -6,11 +6,17 @@ const LangContext = React.createContext();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+function onRenderCallback(...arr) {
+    console.log(arr);
+}
+
 root.render(
   <React.StrictMode>
     <Suspense fallback={<div>Loading...</div>}>
       <LangContext.Provider value={'ru'}>
-        <App />
+        <Profiler id='App' onRender={onRenderCallback}>
+          <App />
+        </Profiler>        
       </LangContext.Provider>
     </Suspense>
   </React.StrictMode>
